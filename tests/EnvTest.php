d14880795ee337193660472249b3284738853f55
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-05 22:39:52 -0500

    Update EnvTest.php

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -1,1 +1,37 @@
-
+<?php
+/**
+ * Genial Framework (https://genial.tech/php/genial-framework/).
+ *
+ * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
+ *
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
+ * @license   https://genial.tech/license/new-bsd New BSD License
+ */
+namespace Genial\Env\Tests;
+use Genial\Env\Env as GenialEnv;
+use PHPUnit\Framework\TestCase;
+/**
+ * EnvTest
+ */
+final class EnvTest extends TestCase
+{
+    /* Example Configuration */
+    private $config = [
+        'session' => [
+            'SESSION_NAME' => 'GenialTest'
+        ]
+    ];
+    /**
+     * getConfigTest().
+     *
+     * @return void
+     */
+    public function getConfigTest()
+    {
+        GenialEnv::setConfig($this->config);
+        $this->assertEquals(
+            'GenialTest',
+            GenialEnv::getConfig('session', 'session_name')
+        );
+    }
+}
