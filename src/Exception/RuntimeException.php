003025f4fb3f2c28f7ba6711f287305bee9b005f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-06 02:14:00 -0500

    Update RuntimeException.php

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -4,8 +4,8 @@
  *
  * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
  *
- * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
- * @license   https://genial.tech/license/new-bsd New BSD License
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://nenglish.me/)
+ * @license   https://nenglish.me/license/new-bsd New BSD License
  */
 
 namespace Genial\Env\Exception;
