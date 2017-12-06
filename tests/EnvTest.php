12ae7bb73c92245a45a9273db09ca7089b93abf5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-06 00:43:54 -0500

    Update EnvTest.php

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -53,4 +53,10 @@ final class EnvTest extends TestCase
         $this->expectException(UnexpectedValueException::class);
         Env::getConfig('');
     }
+    
+    public function testGetConfigFour()
+    {
+        $this->assertEquals(['app_name' => 'Genial'], Env::getConfig('app'));
+    }
+    
 }
