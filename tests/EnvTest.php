1fd182af70e3fd4bf7fa4a36119522253796a934
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-06 02:11:20 -0500

    Update EnvTest.php

--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Genial Framework (https://genial.tech/php/genial-framework/).
+ * Genial Framework (https://nenglish.me/php/genial-framework/).
  *
  * @link      https://github.com/Genial-Framework/Genial-Framework for the canonical source repository
  *
- * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://genial.tech/)
- * @license   https://genial.tech/license/new-bsd New BSD License
+ * @copyright Copyright (c) 2017-2017 Genial Technologies USA Inc. (https://nenglish.me/)
+ * @license   https://nenglish.me/license/new-bsd New BSD License
  */
 
 namespace Genial\Env;
