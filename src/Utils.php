9b6d52a1c10a5e186af3f005698559938717b6f6
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-18 22:45:22 -0500

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -1,1 +1,45 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Env;
+
+use Genial\Env\Exception\UnderflowException;
+use Genial\Env\Config\Config;
+
+/**
+ * Utils
+ */
+class Utils
+{
+    /**
+     * validConfigArray().
+     *
+     * @param array|null $config The config array.
+     *
+     * @throws UnderflowException If the array is empty.
+     *
+     * @return bool True if the array is valid and false if otherwise.
+     */
+    public static function validConfigArray(array $config = array())
+    {
+        if (empty($config)
+        {
+            throw new UnderflowException(sprintf(
+                '`%s` expects `$config` to not be empty.',
+                __METHOD__
+            ));
+        }
+        return Config::validate($config);
+            
+    }
+}
