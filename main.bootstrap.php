2c557c521282a0a367613fb5e4ed44b36d051103
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-20 18:17:55 -0500

    Update main.bootstrap.php

--- main.bootstrap.php
+++ main.bootstrap.php
@@ -72,3 +72,23 @@ function env($section, $variable = null)
 {
     return Env::getConfig($section, $variable);
 }
+
+/**
+ * Create an array depth function
+ */
+function array_depth(array $array)
+{
+    $max_depth = 1
+    foreach ($array as $value)
+    {
+        if (is_array($value))
+        {
+            $depth = array_depth($value) + 1;
+            if ($depth > $max_depth)
+            {
+                $max_depth = $depth;
+            }
+        }
+    }
+    return $max_depth;
+}
