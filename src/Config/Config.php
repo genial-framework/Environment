de07f296e9dd973abbff5f54b2a8f039cf919f16
Nicholas English <nenglish443@gmail.com>
2017-12-20 23:45:38 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -10,11 +10,11 @@
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
  */
-   
+
 namespace Genial\Env\Config;
 
-use Genial\Env\Exception;
 use Genial\Env\Env;
+use Genial\Env\Exception;
 
 /**
  * Config.
@@ -33,26 +33,21 @@ class Config extends Env
      */
     public static function validate(array $xconfig)
     {
-        if (array_depth($xconfig) != 2)
-        {
+        if (array_depth($xconfig) != 2) {
             throw new Exception\DomainException(sprintf(
                 '`%s` The configuration array does nnnot have a depth of 2.',
                 __METHOD__
             ));
         }
-        foreach ($xconfig as $array)
-        {
-            foreach ($array as $var => $val)
-            {
-                if (ctype_upper(str_replace('_', '', $var)))
-                {
+        foreach ($xconfig as $array) {
+            foreach ($array as $var => $val) {
+                if (ctype_upper(str_replace('_', '', $var))) {
                     throw new Exception\UnexpectedValueException(sprintf(
                         '`%s` The configuration variable name must all be caps for it to work properly.',
                         __METHOD__
                     ));
                 }
-                if (strlen($var) > 30 || strlen($val) > 250)
-                {
+                if (strlen($var) > 30 || strlen($val) > 250) {
                     throw new Exception\LengthException(sprintf(
                         '`%s` The `$var` or `$val` variable is too long.',
                         __METHOD__
@@ -61,6 +56,7 @@ class Config extends Env
             }
         }
         self::$config = $xconfig;
+
         return true;
     }
 }
