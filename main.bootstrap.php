89ae1c00302a0aab0b0a1828c06f47c7d1569640
Nicholas English <nenglish443@gmail.com>
2017-12-20 23:19:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- main.bootstrap.php
+++ main.bootstrap.php
@@ -74,21 +74,19 @@ function env($section, $variable = null)
 }
 
 /**
- * Create an array depth function
+ * Create an array depth function.
  */
 function array_depth(array $array)
 {
     $max_depth = 1;
-    foreach ($array as $value)
-    {
-        if (is_array($value))
-        {
+    foreach ($array as $value) {
+        if (is_array($value)) {
             $depth = array_depth($value) + 1;
-            if ($depth > $max_depth)
-            {
+            if ($depth > $max_depth) {
                 $max_depth = $depth;
             }
         }
     }
+
     return $max_depth;
 }
