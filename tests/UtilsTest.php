6da1e48fc3cd45985487d8d7862f75f5902ceb18
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-20 20:01:20 -0500

    Update UtilsTest.php

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -1,1 +1,41 @@
-
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
+ */
+   
+namespace Genial\Env;
+   
+use PHPUnit\Framework\TestCase;
+   
+/**
+ * EnvTest.
+ */
+final class UtilsTest extends TestCase
+{
+    public function test()
+    {
+        $this->expectException(Exception\UnderflowException::class);
+        Utils::validConfigArray();
+    }
+   
+    public function test2()
+    {
+        $this->expectException(Exception\UnderflowException::class);
+        $this->assertTrue(Utils::validConfigArray([
+            'test' => [
+                'FOO' => 'bar'
+            ]
+        ]));
+    }
+    function __destruct()
+    {
+        Env::clearConfig();
+    }
+}
