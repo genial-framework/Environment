8052eddd1e0cff46e85d3d73b769c83ae279dd13
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-26 23:39:51 -0500

    Update Env.php

--- src/Env.php
+++ src/Env.php
@@ -6,7 +6,6 @@
  * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
  *
  * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
- *
  * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
  */
@@ -18,6 +17,7 @@ namespace Genial\Env;
  */
 class Env
 {
+    
     /**
      * @var array|null The configuration array
      */
@@ -33,7 +33,7 @@ class Env
      *
      * @throws BadMethodCallException   If the $section argument is missing
      * @throws UnexpectedValueException If the $section argument is empty
-     * @throws OverflowException        If the $section argument does not exist
+     * @throws OverflowException        If the $section name does not exist
      * @throws RuntimeException         If there is no configuration array set
      * @throws LengthException          If the request vriables are too long
      *
@@ -41,44 +41,52 @@ class Env
      */
     public static function getConfig(string $section = null, string $variable = null)
     {
-        if (is_null($section)) {
+        if (is_null($section))
+        {
             throw new Exception\BadMethodCallException(sprintf(
                 '`%s` The `$section` argument is missing.',
                 __METHOD__
             ));
         }
         $section = trim($section);
-        if (empty($section) || $section == '') {
+        if (empty($section) || $section == '')
+        {
             throw new Exception\UnexpectedValueException(sprintf(
                 '`%s` The `$section` argument is empty.',
                 __METHOD__
             ));
         }
-        if (strlen($section) > 30 || strlen($variable) > 250) {
+        if (strlen($section) > 30 || strlen($variable) > 250)
+        {
             throw new Exception\LengthException(sprintf(
                 '`%s` The `$section` or `$variable` variable is too long.',
                 __METHOD__
             ));
         }
-        if (!self::$config) {
+        if (!self::$config)
+        {
             throw new Exception\RuntimeException(sprintf(
                 '`%s` the `self::$config` is not set.',
                 __METHOD__
             ));
-        } else {
+        } else
+        {
             $env = self::$config;
         }
-        if (isset($env[$section])) {
-            foreach ($env[$section] as $nVariable => $value) {
-                if (!is_null($variable) && $nVariable == strtoupper($variable)) {
+        if (isset($env[$section]))
+        {
+            foreach ($env[$section] as $nVariable => $value)
+            {
+                if (!is_null($variable) && $nVariable == strtoupper($variable))
+                {
                     return $value;
                 }
-                if (!$variable) {
+                if (!$variable)
+                {
                     goto doReturn;
                 }
             }
         }
-
         throw new Exception\UnderflowException(sprintf(
             '`%s` The request section name is not in the configuration array.',
             __METHOD__
@@ -96,7 +104,7 @@ class Env
      *
      * @throws InvalidArgumentException If $config is not an array
      *
-     * @return bool|true if configuration array was set
+     * @return bool|true True if the configuration array is set.
      */
     public static function setConfig($config = [])
     {
@@ -106,7 +114,6 @@ class Env
                 __METHOD__
             ));
         }
-
         return Utils::validConfigArray($config);
     }
 
@@ -120,7 +127,7 @@ class Env
     public static function clearConfig()
     {
         self::$config = false;
-
         return false;
     }
+    
 }
