e853685e22640706df615a0932156be12cd227f7
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-28 17:51:03 -0500

    Update FormatInterface.php

--- src/FormatInterface.php
+++ src/FormatInterface.php
@@ -1,1 +1,31 @@
-
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
+ */
+   
+namespace Genial\Env;
+   
+/**
+ * FormatInterface.
+ */
+interface FormatInterface
+{
+  
+    /**
+     * __invoke().
+     *
+     * Set the configuration.
+     *
+     * @param mixed The value to format.
+     *
+     * @return string The formatted value.
+     */
+    public function __invoke($xvalue);
+  
+}
