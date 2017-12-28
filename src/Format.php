96dd80d58d983124f5af2f5a3887d77b098300f6
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-28 17:18:40 -0500

    Update Format.php

--- src/Format.php
+++ src/Format.php
@@ -1,1 +1,41 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Env;
+
+/**
+ * Format.
+ */
+class Format implements FormatInterface
+{
+  
+    /**
+     * @var array $config The configuration array
+     */
+    protected static $config;  
+  
+    /**
+     * __invoke().
+     *
+     * Set the configuration.
+     *
+     * @param array $xconfig The configuration array.
+     *
+     * @return bool|true If the configuration was set.
+     */
+    public function __invoke($xconfig)
+    {
+        self::$config = $xconfig;
+        return true;
+    }
+  
+}
