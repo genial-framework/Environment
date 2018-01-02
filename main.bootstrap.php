69a2e26eb7b750ac85a4282e244704f61a1957a7
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-02 14:57:31 -0500

    Update main.bootstrap.php

--- main.bootstrap.php
+++ main.bootstrap.php
@@ -26,7 +26,7 @@ $defaultConfig = [
 if (defined('APP_ROOT') && file_exists(APP_ROOT . '/.env.ini'))
 {
     $env = parse_ini_file(APP_ROOT . '/.env.ini', true, INI_SCANNER_RAW);
-    Env::setConfig($env);
+    Env::setConfig(Formatter::initialize($env));
 } else
 {
     Env::setConfig($defaultConfig);
