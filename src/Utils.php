7658d65e203a86daf6b24f6fb0348c5a90f0762d
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-02 02:21:27 -0500

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -1,36 +1,7 @@
 <?php
-/**
- * Genial Framework.
- *
- * @author    Nicholas English <https://github.com/Nenglish7>
- * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
- *
- * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
- * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
- */
-
 namespace Genial\Env;
-
-use Genial\Env\Config\Config;
-
-/**
- * Utils.
- */
 class Utils
 {
-    
-    /**
-     * validConfigArray().
-     *
-     * Validate the configuration array.
-     *
-     * @param array|null $config The config array.
-     *
-     * @throws UnderflowException If the array is empty.
-     *
-     * @return bool|true True if the configuration array is valid.
-     */
     public static function validConfigArray(array $config = [])
     {
         if (empty($config))
@@ -42,5 +13,4 @@ class Utils
         }
         return Config::validate($config);
     }
-    
 }
