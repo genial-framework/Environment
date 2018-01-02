a9d0d9567635dda30467596851e6b93827bbc80e
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-02 16:07:46 -0500

    Update and rename Formatter.php to FormatterTest.php

---
+++ tests/FormatterTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
+ */
+
+namespace Genial\Env;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * FormatterTest.
+ */
+final class FormatterTest extends TestCase
+{
+    
+    /**
+     * testInitialize().
+     *
+     * Test the random int function.
+     *
+     * @return void.
+     */
+    public function testInitialize()
+    {
+        $this->assertTrue(is_array(Formatter::initialize([
+            'application' => [ 
+            ],
+        ])));
+    }
+  
+    /**
+     * testInitialize2().
+     *
+     * Test the random int function.
+     *
+     * @return void.
+     */
+    public function testInitialize2()
+    {
+        $this->assertTrue(is_array(Formatter::initialize([
+            'application' => [
+                'APP_SECRET_KEY' => '[GENIAL]@%$-<:Ns3Z-3^rUc>YdF$',
+                'APP_NAME'       => 'Genial',
+                'DEBUG'          => false,
+                'LOG'            => true,
+            ],
+        ])));
+    }
+  
+    /**
+     * testInitialize3().
+     *
+     * Test the random int function.
+     *
+     * @return void.
+     */
+    public function testInitialize3()
+    {
+        $this->assertTrue(is_array(Formatter::initialize([
+            'application' => [
+                'APP_SECRET_KEY' => '[GENIAL]@%$-<:Ns3Z-3^rUc>YdF$',
+                'APP_NAME'       => 'Genial',
+                'DEBUG'          => false,
+                'LOG'            => true,
+            ],
+            'test' => [
+                'FOO' => 'null',
+                'BAR' => 'true',
+                'MEO' => 'false',
+                'BAT' => '1',
+                'FUN' => '0',
+            ],
+        ])));
+    }
+  
+}
