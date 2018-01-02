6ba7f187f04432b36eb46085048bb0410435aa25
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-02 14:15:37 -0500

    Update Key.php

--- src/Key.php
+++ src/Key.php
@@ -15,8 +15,57 @@ namespace Genial\Env;
 /**
  * Key.
  */
-class Key 
+class Key implements KeyInterface
 {
     
-
+    /**
+     * @var string|[GENIAL]@ $prefix The key prefix.
+     */
+    private $prefix = '[GENIAL]@';
+    
+    /**
+     * generateKey().
+     *
+     * Generate a secret key for Genial Framework
+     *
+     * @param int|20 $keyLength     The key length.
+     * @param string|null $keyspace The characters that should be used in generation.
+     *
+     * @throws LengthException If the request variables are too long or too short.
+     *
+     * @return string The generated key.
+     */
+    public function generateKey($keyLength = 20, $keySpace = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+=<>,.{}[]|\;:?/`~')
+    {
+        if ($keyLength < 15 || $keyLength > 250)
+        {
+            throw new Exception\LengthException(sprintf(
+                '`%s` The `$keyLength` needs to be between `15` and `250`.',
+                __METHOD__
+            ));
+        }
+        return $this->prefix . $this->generateRandomString($keyLength, $keySpace);
+    }
+    
+    /**
+     * generateRandomString().
+     *
+     * Generate a random string
+     *
+     * @param int|20 $keyLength     The string length.
+     * @param string|null $keyspace The characters that should be used in generation.
+     *
+     * @return string The generated key.
+     */
+    public function generateKey($stringLength = 20, $keySpace = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_-+=<>,.{}[]|\;:?/`~')
+    {
+        $string = '';
+        $max = mb_strlen($keySpace, '8bit') - 1;
+        for ($i = 0; $i < $stringLength; ++$i)
+        {
+            $string .= $keySpace[random_int(0, $max)];
+        }
+        return $string;
+    }
+     
 }
