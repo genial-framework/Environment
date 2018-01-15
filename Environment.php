8454e1e8bfb324a472f07ba067d40bbbd77f1e7b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 17:49:49 -0500

    Update Environment.php

--- Environment.php
+++ Environment.php
@@ -1,1 +1,16 @@
-
+/**
+ * @link      <https://github.com/SyzerPHP/Environment> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <https://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
+ */
+namespace SyzerPHP\Environment;
+/**
+ * Environment.
+ */
+class Environment
+{
+    /**
+     * @var array|null $conf The config array.
+     */
+    protected static $conf = null;
+}
