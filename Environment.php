0ad98b4c48c7494ee64b7111e0e3100f14c22101
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 18:13:17 -0500

    Update Environment.php

--- Environment.php
+++ Environment.php
@@ -14,4 +14,75 @@ class Environment
      * @var array|null $conf The config array.
      */
     protected static $conf = \null;
+    /**
+     * getConfig().
+     *
+     * Get a config section or section variable name.
+     *
+     * @param string $sec      The config section.
+     * @param string|null $var The section variable name.
+     *
+     * @throws DomainException          If the `$sec` argument is empty.
+     * @throws LengthException          If the `$sec`/`$var` argument is too long.
+     * @throws RuntimeException         If there is no config array set.
+     * @throws UnexpectedValueException If the section name is not in the config array.
+     *
+     * @return array Return the section array or section variable name.
+     */
+    public static function getConfig(string $sec, string $var = null): string
+    {
+        $sec = \trim($sec);
+        if (\empty($sec) || $sec == '') {
+            throw new Exception\DomainException('The `$sec` argument is empty.');
+        }
+        if (\strlen($sec) > 30 || \strlen($var) > 250) {
+            throw new Exception\LengthException(\sprintf(
+                'The `$section` and/or `$variable` argument is too long. Passed: `$sec` = `%s` `$var` = `%s`.',
+                \strlen($sec),
+                \strlen($var)
+            ));
+        }
+        if (!self::$conf) {
+            throw new Exception\RuntimeException('The `self::$conf` class variable is not set.');
+        } else {
+            $env = self::$conf;
+        }
+        if (isset($env[$sec])) {
+            foreach ($env[$sec] as $var2 => $val) {
+                if (!\is_null($var) && $var2 == \strtoupper($var)) {
+                    return \strval($val);
+                }
+            }
+        } else {
+            throw new Exception\UnexpectedValueException(\sprintf(
+                'The section name is not in the config array. Passed: `%s`.',
+                \htmlspecialchars($sec, \ENT_QUOTES, 'UTF-8')
+            ));
+        }
+        return $env[$sec];
+    }
+    /**
+     * setConfig().
+     *
+     * Set the config array.
+     *
+     * @param array $conf The config array.
+     *
+     * @return void.
+     */
+    public static function setConfig(array $conf): void
+    {
+        Config::validate($conf);
+    }
+    /**
+     * clearConfig().
+     *
+     * Clear the config array.
+     *
+     * @return void.
+     */
+    public static function clearConfig(): void
+    {
+        self::$conf = \null;
+    }
 }
