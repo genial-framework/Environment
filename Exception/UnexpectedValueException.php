5b2098c7a0ffc8beb8d489212ed95b95d79a59fc
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 17:28:50 -0500

    Update UnexpectedValueException.php

--- Exception/UnexpectedValueException.php
+++ Exception/UnexpectedValueException.php
@@ -2,7 +2,7 @@
 /**
  * @link      <https://github.com/SyzerPHP/Environment> for the canonical source repository.
  * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
- * @license   <hztps://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
+ * @license   <https://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
  */
 namespace SyzerPHP\Environment\Exception;
 /**
