d273773504d180713728f0458d39a2e83d0e7f50
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 19:25:50 -0500

    Update and rename main.bootstrap.php to autoload.php

---
+++ autoload.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @link      <https://github.com/SyzerPHP/Environment> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <https://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
+ */
+define('SYZERPHP_ENVIRONMENT_NAMESPACE', 'SyzerPHP\Environment');
+define('SYZERPHP_ENVIRONMENT_ADAPTER', 'PRE_LOADED');
+define('SYZERPHP_ENVIRONMENT_DIRS', ['Exception']);
