dee2a36069295280802606865a860d700dd42c10
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 20:13:11 -0500

    Update ConfigTest.php

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,138 +1,24 @@
 <?php
 /**
- * Genial Framework.
- *
- * @author    Nicholas English <https://github.com/Nenglish7>
- * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
- *
- * @link      <https://github.com/Genial-Framework/Env> for the canonical source repository.
- * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Env/blob/master/LICENSE> New BSD License.
+ * @link      <https://github.com/SyzerPHP/Environment> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <https://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
  */
-
-namespace Genial\Env;
-
-use PHPUnit\Framework\TestCase;
-
+namespace SyzerPHP\Environment;
+use \PHPUnit\Framework\TestCase;
 /**
  * ConfigTest.
  */
 final class ConfigTest extends TestCase
 {
-    
-    /**
-     * testValidate().
-     *
-     * Test the validate function.
-     *
-     * @return void
-     */
     public function testValidate()
     {
         $this->expectException(Exception\DomainException::class);
-        Config::validate([
-            'hello' => 'world'
-        ], 1);
+        Config::validate([]); /* Pass empty array */
     }
-
-    /**
-     * testValidate2().
-     *
-     * Test the validate function.
-     *
-     * @return void
-     */
-    public function testValidate2()
-    {
-        $this->expectException(Exception\UnexpectedValueException::class);
-        Config::validate([
-            'test' => [
-                'TEST' => 'config',
-            ],
-            'hello' => 'world'
-        ], 1);
-    }
-
-    /**
-     * testValidate3().
-     *
-     * Test the validate function.
-     *
-     * @return void
-     */
-    public function testValidate3()
-    {
-        $this->expectException(Exception\UnexpectedValueException::class);
-        Config::validate([
-            'test' => [
-                'hello' => 'bar',
-            ],
-        ], 1);
-    }
-    
-    /**
-     * testValidate4().
-     *
-     * Test the validate function.
-     *
-     * @return void
-     */
-    public function testValidate4()
+    public function testValidate()
     {
         $this->expectException(Exception\DomainException::class);
-        Config::validate([
-            'test' => [
-                'test' => [
-                    'FOO' => 'bar',
-                ],
-            ],
-        ], 1);
+        Config::validate(['hello' => 'world']); /* Modify array depth */
     }
-
-    /**
-     * testValidate5().
-     *
-     * Test the validate function.
-     *
-     * @return void
-     */
-    public function testValidate5()
-    {
-        $this->expectException(Exception\LengthException::class);
-        Config::validate([
-            'test' => [
-                'WIUCHNUIWFCINQOWDIJNOIQJNWDOIJNCQIWNJDOICNJOIQWJNDCOIJNW' => 'foobar',
-            ],
-        ], 1);
-    }
-
-    /**
-     * testValidate6().
-     *
-     * Test the validate function.
-     *
-     * @return void
-     */
-    public function testValidate6()
-    {
-        Env::clearConfig();
-        $this->assertTrue(Config::validate([
-            'test' => [
-                'FOO' => 'bar',
-            ],
-        ], 1));
-    }
-
-    /**
-     * __destruct().
-     *
-     * Clear the config
-     *
-     * @return void
-     */
-    public function __destruct()
-    {
-        Env::clearConfig();
-    }
-    
 }
