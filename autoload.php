4748a9f89791ea5ec9697023ff842fb0a2bc82ee
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 20:27:42 -0500

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -7,3 +7,18 @@
 define('SYZERPHP_ENVIRONMENT_NAMESPACE', 'SyzerPHP\Environment');
 define('SYZERPHP_ENVIRONMENT_ADAPTER', 'PRE_LOADED');
 define('SYZERPHP_ENVIRONMENT_DIRS', ['Exception']);
+function e($data)
+{
+    return htmlspecialchars((string) $data, ENT_QUOTES, 'UTF-8');
+}
+function depth($var)
+{
+    $depth = 1;
+    foreach ($var as $val) {
+        if (is_array($val)) {
+            $depth += depth($val);
+            break;
+        }
+    }
+    return $depth; 
+}
