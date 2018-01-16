cbbc4f6fbbce746734a54ac9ddd50908b03db2a5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 19:16:09 -0500

    Update Key.php

--- Key.php
+++ Key.php
@@ -1,1 +1,28 @@
-
+<?php
+/**
+ * @link      <https://github.com/SyzerPHP/Environment> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <https://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
+ */
+namespace SyzerPHP\Environment;
+/**
+ * Key.
+ */
+class Key
+{
+    /**
+     * @var string|%% $prefix The key prefix.
+     */
+    private $prefix = '[SYZER]@';
+    /**
+     * generateKey().
+     *
+     * Generate a secret key for SyzerPHP.
+     *
+     * @return string The generated key.
+     */
+    public function generateKey(): string
+    {
+        return (string) $this->prefix . \bin2hex(\random_bytes(10));
+    }
+}
