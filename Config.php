27522843b8d5b594affa6130b7bb73ac01272bd6
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-15 19:06:14 -0500

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,1 +1,65 @@
-
+<?php
+/**
+ * @link      <https://github.com/SyzerPHP/Environment> for the canonical source repository.
+ * @copyright Copyright (c) 2018, SyzerPHP <https://github.com/SyzerPHP>.
+ * @license   <https://github.com/SyzerPHP/Environment/blob/master/LICENSE> New BSD License.
+ */
+namespace SyzerPHP\Environment;
+/**
+ * Config.
+ */
+class Config extends Environment
+{
+    /**
+     * validate().
+     *
+     * Validate the config array.
+     *
+     * @param array $conf The config array.
+     *
+     * @throws DomainException          If `$conf` is empty.
+     * @throws DomainException          If `$conf` does not have a depth of 2.
+     * @throws UnexpectedValueException If a section is not the start of an array.
+     * @throws UnexpectedValueException If the section variable names are not capital letters.
+     * @throws LengthException          If the section variable name and/or value is too long.
+     *
+     * @return void.
+     */
+    public static function validate(array $conf)
+    {
+        if (\empty($conf)) {
+            throw new Exception\DomainException('The config array is empty.');
+        }
+        if (\array_depth($conf) != 2) {
+            throw new Exception\DomainException(\sprintf(
+                'The config array does not have a depth of 2. Depth: `%s`.',
+                (string) \array_depth($conf)
+            ));
+        }
+        foreach ($conf as $var => $val) {
+            if (!\is_array($val)) {
+                throw new Exception\UnexpectedValueException(\sprintf(
+                    'The section is not the start of an array. Passed: `%s`.',
+                    \htmlspecialchars($var, \ENT_QUOTES, 'UTF-8')
+                ));
+            }
+            foreach ($val as $var2 => $val2) {
+                if (!\ctype_upper(\str_replace('_', '', $var2))) {
+                    throw new Exception\UnexpectedValueException(\sprintf(
+                        'The section variable name must all be caps. Passed: `%s`.',
+                        \htmlspecialchars($var2, \ENT_QUOTES, 'UTF-8')
+                    ));
+                }
+                if (\strlen($var2) > 30 || \strlen($val2) > 250) {
+                    throw new Exception\LengthException(sprintf(
+                        'The `$var2` and/or `$val2` variable is too long. Passed: `$var2` = `%s` `$val2` = `%s`.',
+                        (string) \strlen($var2),
+                        (string) \strlen($val2)
+                    ));
+                }
+            }
+        }
+        self::clearConfig();
+        self::$conf = $conf;
+    }
+}
